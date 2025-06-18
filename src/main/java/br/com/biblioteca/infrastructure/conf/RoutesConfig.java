668fdd1862ed0c41c9d36a887dba134034c59ac8
miguelamaral254 <miguel.amaral.sales@gmail.com>
2025-06-18 17:27:41 -0300

    subindo assim mesmo

--- src/main/java/br/com/biblioteca/infrastructure/conf/RoutesConfig.java
+++ src/main/java/br/com/biblioteca/infrastructure/conf/RoutesConfig.java
@@ -8,24 +8,25 @@ public class RoutesConfig {
     public static final String[] PUBLIC_ROUTES = {
             "swagger-ui/index.html",
             "/auth/**",
+            "/users/**",
 
 
     };
     public static final String[] USER_ROUTES = {
             "/influenza-nordeste/**",
             "/leitos-sus-nao-sus/**",
-            "/users/**",
+
     };
     public static final String[] RESEARCHER = {
             "/influenza-nordeste/**",
             "/leitos-sus-nao-sus/**",
-            "/users/**",
+
     };
 
     public static final String[] ADMIN_ROUTES = {
             "/influenza-nordeste/**",
             "/leitos-sus-nao-sus/**",
-            "/users/**",
+
     };
     public static final HttpMethod[] PUBLIC_HTTP_METHODS = {
             HttpMethod.GET, HttpMethod.POST
